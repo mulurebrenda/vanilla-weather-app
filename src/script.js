5f446a763548d1e71e2bd92db33e82966ab8262f
mulurebrenda <144659497+mulurebrenda@users.noreply.github.com>
2023-10-29 16:00:12 +0300

    updated the date and time using js

---
+++ src/script.js
@@ -0,0 +1,30 @@
+let now = new Date();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let time = document.querySelector("#time");
+time.innerHTML = `${hours}:${minutes}`;
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let year = now.getFullYear();
+let fullDate = document.querySelector("#date");
+fullDate.innerHTML = `${month} ${date}, ${year}`;
