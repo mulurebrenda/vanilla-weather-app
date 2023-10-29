5c3ed5b5883b7733f197bddd232bfaa11835f677
mulurebrenda <144659497+mulurebrenda@users.noreply.github.com>
2023-10-29 20:34:40 +0300

    updated html and added responsivity using js

--- src/script.js
+++ src/script.js
@@ -28,3 +28,41 @@ let date = now.getDate();
 let year = now.getFullYear();
 let fullDate = document.querySelector("#date");
 fullDate.innerHTML = `${month} ${date}, ${year}`;
+
+let apiKey = "05fo015e85414d77adb5a43ddt2314b8";
+function showTemperature(response) {
+  console.log(response);
+debugger
+  //rounding off the temperature
+  let temperature = Math.round(response.data.temperature.current);
+  let temperatureMax = Math.round(response.data.main.temp_max);
+  let temperatureMin = Math.round(response.data.main.temp_min);
+ 
+  let location = document.querySelector("#location");
+  location.innerHTML = `${response.data.city}`;
+  let city = document.querySelector("#city");
+  city.innerHTML = `${response.data.city}, ${response.data.country}`;
+  let currentTemperature = document.querySelector("#current-temperature");
+  currentTemperature.innerHTML = `${temperature}°`;
+  let highestTemperature = document.querySelector("#highest-temp");
+  highestTemperature.innerHTML = `${temperatureMax}°`;
+  let lowestTemperature = document.querySelector("#lowest-temp");
+  lowestTemperature.innerHTML = `${temperatureMin}°`;
+  let weatherDescription = document.querySelector("#weather-description");
+  weatherDescription.innerHTML = response.data.condition.description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = `${response.data.wind.speed}m/s`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input-text");
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
